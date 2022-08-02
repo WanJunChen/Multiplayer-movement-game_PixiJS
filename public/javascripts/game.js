522e1c396e6828722f8c9d8edb131dca13328d06
WanJunChen <92075248+WanJunChen@users.noreply.github.com>
2022-08-02 18:05:19 +0800

    在畫面中加入其他連線的玩家
    已完成 : 追蹤每位連線玩家的座標
                   otherPlayer中存放除了自己以外的其他玩家資料
    還沒寫 : "玩家離線時移除玩家"的程式

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,15 +1,13 @@
 const socket = io.connect();
 
 let myPlayer, state;
-let AllPlayer = [];
+let otherPlayer = [];
 let PlayerImageTexture;
+
 let PlayerInfo = {
     name: prompt('請輸入名稱: '),
-    ImageTexture: PlayerImageTexture,
     x: 530,
     y: 150,
-    vx: 0,
-    vy: 0,
     width: 50,
     height: 75,
     id: null
@@ -44,27 +42,49 @@ function setup() {
     setGameScene(1600, gameHeight);
     
     // ========== Set all player on map ==========
-    PlayerInfo.ImageTexture = PIXI.Texture.from("/images/character.png");
+    //寫socket.io --2022/8/1 下午11:20
     PlayerInfo.id = GetID();
+    socket.emit('initialize', PlayerInfo);
+    
     myPlayer = createPlayer(PlayerInfo);
     app.stage.addChild(myPlayer);
 
-    // 發送一個 "sendMessage" 事件
-    socket.emit("sendMessage", { 
-        Player: {
-            id: PlayerInfo.id,
-            name: PlayerInfo.name,
-            width:  PlayerInfo.width,
-            height:  PlayerInfo.height,
-            TextureFrom: PlayerInfo.ImageTexture.textureCacheIds[0]
-        }
-    });
-    // 監聽來自 server 的 "allMessage" 事件
-    socket.on("allMessage", function (message) {
-        console.log(message)
-    });
+    socket.on("AddPlayer", SaveOtherPlayer);
+    socket.on("RemovePlayer", SaveOtherPlayer);
+    // for(let i = 0; i < otherPlayer.length; i++) {
+        
+    // }
 
+    setInterval(() => {
+        socket.emit('update', myPlayer.id, myPlayer.x, myPlayer.y);
+    }, 500);
+    socket.on('updateAllData', function(id, x, y) {
+        
+    });
     PlayerMove(myPlayer);
+
+    // Update Player Data
+    // setInterval(() => {
+
+    // }, 100);
+
+    // // 發送一個 "sendMessage" 事件
+    // socket.emit("sendMessage", { 
+    //     Player: {
+    //         id: PlayerInfo.id,
+    //         name: PlayerInfo.name,
+    //         width:  PlayerInfo.width,
+    //         height:  PlayerInfo.height,
+    //         TextureFrom: PlayerInfo.ImageTexture.textureCacheIds[0]
+    //     }
+    // });
+    // // 監聽來自 server 的 "allMessage" 事件
+    // socket.on("allMessage", function (message) {
+    //     console.log(message)
+    // });
+
+
+
 
     state = play;
     app.ticker.add(delta =>gameLoop(delta));
@@ -84,55 +104,7 @@ function play(delta) {
 function end() {
     //All the code that should run at the end of the game
 }
-
-
-//The 'keyboard' helper function
-function keyboard(keyCode) {
-    var key = {};
-    key.code = keyCode;
-    key.isDown = false;
-    key.isUp = true;
-    key.press = undefined;
-    key.release = undefined;
-    key.string = "";
-    if(keyCode == 37) key.string = "LEFT";
-    else if(keyCode == 38) key.string = "UP";
-    else if(keyCode == 39) key.string = "RIGHT";
-    else if(keyCode == 40) key.string = "DOWN";
-    
-    //The 'down Handler'
-    key.downHandler = event => {
-      if (event.keyCode === key.code) {
-        if (key.isUp && key.press) {
-            console.log(myPlayer.name, myPlayer.id, ", Move direction:", key.string);
-            key.press();
-        }
-        key.isDown = true;
-        key.isUp = false;
-      }
-
-      event.preventDefault();
-    };
   
-    //The 'up Handler'
-    key.upHandler = event => {
-      if (event.keyCode === key.code) {
-        if (key.isDown && key.release) {
-            key.release()
-        };
-        key.isDown = false;
-        key.isUp = true;
-      }
-      event.preventDefault();
-    };
-  
-    //Attach event listeners
-    window.addEventListener( "keydown", key.downHandler.bind(key), false );
-    window.addEventListener( "keyup", key.upHandler.bind(key), false );
-
-    return key;
-}
-
 function PlayerMove(Player) { 
     let speed = 5;
     //Left arrow key `press` method
@@ -186,6 +158,53 @@ function PlayerMove(Player) {
 
     
 }
+
+//The 'keyboard' helper function
+function keyboard(keyCode) {
+    var key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    key.string = "";
+    if(keyCode == 37) key.string = "LEFT";
+    else if(keyCode == 38) key.string = "UP";
+    else if(keyCode == 39) key.string = "RIGHT";
+    else if(keyCode == 40) key.string = "DOWN";
+    
+    //The 'down Handler'
+    key.downHandler = event => {
+      if (event.keyCode === key.code) {
+        if (key.isUp && key.press) {
+            // console.log(myPlayer.name, myPlayer.id, ", Move direction:", key.string);
+            key.press();
+        }
+        key.isDown = true;
+        key.isUp = false;
+      }
+
+      event.preventDefault();
+    };
+  
+    //The 'up Handler'
+    key.upHandler = event => {
+      if (event.keyCode === key.code) {
+        if (key.isDown && key.release) {
+            key.release()
+        };
+        key.isDown = false;
+        key.isUp = true;
+      }
+      event.preventDefault();
+    };
+  
+    //Attach event listeners
+    window.addEventListener( "keydown", key.downHandler.bind(key), false );
+    window.addEventListener( "keyup", key.upHandler.bind(key), false );
+
+    return key;
+}
   
 function GetID(){
     return socket.id;
@@ -205,6 +224,7 @@ function setBagBar(width, height) {
     BagTitle.x = (width - BagTitle.width) / 2 + 10;
     BagTitle.y = 50;
     BagBar.addChild(BagTitle);
+    BagBar.name = 'BagBar';
     for (let i = 0; i < ItemCount; i++){
         let bagItem = new PIXI.Graphics();
         bagItem.beginFill(0xFFFFFF);
@@ -218,6 +238,7 @@ function setBagBar(width, height) {
 
 function setGameScene(width, height) {
     let gameScene =  new PIXI.Container();
+    gameScene.name = 'gameScene';
     let gameBG = new PIXI.Sprite(app.loader.resources["/images/Achievement_map.jpg"].texture);
     gameBG.x = 200;
     gameBG.y = 10;
@@ -229,9 +250,9 @@ function setGameScene(width, height) {
 
 function createPlayer(PlayerInfo) {
     let PlayerContainer = new PIXI.Container();
-    let Player = new PIXI.Sprite(PlayerInfo.ImageTexture);
+    let Player = new PIXI.Sprite(PIXI.Texture.from("/images/character.png"));
     let Name = new PIXI.Text(PlayerInfo.name, {
-        fontSize: 20,
+        fontSize: 18,
         fill: 0xFFFFFF
     });
     let NameBG = new PIXI.Graphics()
@@ -244,16 +265,49 @@ function createPlayer(PlayerInfo) {
     PlayerContainer.id = PlayerInfo.id;
     PlayerContainer.x = PlayerInfo.x;
     PlayerContainer.y = PlayerInfo.y;
-    PlayerContainer.vx = PlayerInfo.vx;
-    PlayerContainer.vy = PlayerInfo.vy;
+    PlayerContainer.vx = 0;
+    PlayerContainer.vy = 0;
     Player.width = PlayerInfo.width;
     Player.height = PlayerInfo.height;
     Player.x = 0;
     Player.y = 25;
     Name.x = (PlayerInfo.width - Name.width) / 2;
-    Name.y = 0;
+    Name.y = 1;
     PlayerContainer.addChild(NameBG);
     PlayerContainer.addChild(Name);
     PlayerContainer.addChild(Player);
     return PlayerContainer;
+}
+let a = 50
+function SaveOtherPlayer(Players) { // Player(陣列)裡面放的是場上目前所有玩家的資料
+    otherPlayer.length = 0;
+    let isIncludeInApp = false;
+    let thePlayer;
+    for (var i = 0; i < Players.length; i++) {
+        if(Players[i].id != socket.id) {
+            otherPlayer.push(Players[i]);
+        }
+    }
+    // 判斷otherPlayer中的玩家是否存在於app.stage中，若沒有，將此玩家addChild()進app.stage
+    for (var i = 0; i < otherPlayer.length; i++) {
+        isIncludeInApp = false;
+        for (var j = 0; j < app.stage.children.length; j++) {
+            if (app.stage.children[j].id != undefined) {
+                if (otherPlayer[i].id == app.stage.children[j].id) {
+                    isIncludeInApp = true;
+                    break;
+                }
+            }
+        }
+        if (isIncludeInApp == false) {
+            thePlayer = createPlayer(otherPlayer[i]);
+            thePlayer.x += a;
+            a += 50;
+            app.stage.addChild(thePlayer);
+        }
+    }
+
+    //接下來還要寫移除玩家的程式 --2022/8/2
+
+    console.log('現在有',(otherPlayer.length+1), '個人');
 }
